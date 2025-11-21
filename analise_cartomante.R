4f88a7feeb46ee278e34de0c1d9502478f2dfcc0
sabrinaevelyn1 <sabrina.evelyn@unesp.br>
2025-11-21 14:02:01 -0300

    adiciona contagem de palavras do conto

--- analise_cartomante.R
+++ analise_cartomante.R
@@ -5,3 +5,20 @@ library(tidyverse)
 
 # Lendo o arquivo de texto
 texto <- read_lines("cartomante.txt")
+
+# transformar o texto em um único texto
+texto_unico <- paste(texto, collapse = "")
+
+#minusculo
+texto_unico <- tolower(texto_unico)
+
+#tirar pontuacao 
+texto_limpo <- texto_unico %>% 
+  str_replace_all("[[:punct:]]", "")
+
+# separar palavras
+palavras <- str_split(texto_limpo, "\\s+") [[1]]
+
+# ver quantas palavras tem no texto
+
+length(palavras)
