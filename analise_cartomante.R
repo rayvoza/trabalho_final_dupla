68135536976b160f6148cba6d07ebb9d2cd58bb1
RayVoza <raiane.pereira@unesp.br>
2025-11-21 14:24:22 -0300

    estatísticas básicas

--- analise_cartomante.R
+++ analise_cartomante.R
@@ -22,3 +22,30 @@ palavras <- str_split(texto_limpo, "\\s+") [[1]]
 # ver quantas palavras tem no texto
 
 length(palavras)
+
+
+# -----------------------------
+# ESTATÍSTICAS BÁSICAS DO TEXTO
+# -----------------------------
+
+# 1. Número de linhas
+num_linhas <- length(texto)
+num_linhas
+
+# 2. Número de palavras
+num_palavras <- length(palavras)
+num_palavras
+
+# 3. Número total de caracteres
+num_caracteres <- sum(nchar(texto_limpo))
+num_caracteres
+
+# 4. Tamanho de cada palavra
+tamanhos <- nchar(palavras)
+
+# 5. Média e mediana do tamanho das palavras
+media_tamanho <- mean(tamanhos)
+mediana_tamanho <- median(tamanhos)
+
+media_tamanho
+mediana_tamanho
